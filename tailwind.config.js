722d9e9d09c45185c9e74c20db8a11786088e03b
arifur rahman <arifurrex@gmail.com>
2023-06-25 01:12:20 +0600

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      primary: 'Orbitron',
-      secondary: 'Rajdhani',
-      tertiary: 'Aldrich',
+      primary: "Orbitron",
+      secondary: "Rajdhani",
+      tertiary: "Aldrich",
     },
-    container:{
+    container: {
       padding: {
-         DEFAULT:'15px'
-      }
+        DEFAULT: "15px",
+      },
     },
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     extend: {
       colors: {
-        primary: '#0a0a0a',
-        accent: '#B809C3',
+        primary: "#0a0a0a",
+        accent: "#B809C3",
       },
       backgroundImage: {
-        site: "url('./src/assets/site-bg.jpg')",
+        site: "url('./image/site-bg.jpg')",
         about: "url('./src/assets/arifur.jpg')",
         services: "url('./src/assets/programming.jpg')",
-        
-      }
+      },
     },
   },
   plugins: [],
-}
+};
