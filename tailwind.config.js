1b4d5fe46e72fb789628fa79697271a9980e9dda
arifur rahman <arifurrex@gmail.com>
2023-07-06 11:53:22 +0600

    bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,12 @@ export default {
         primary: "#0a0a0a",
         accent: "#B809C3",
       },
-      backgroundImage: {
+      backgroundImage: (theme) => ({
         // site: "url(https://images.unsplash.com/photo-1631694109908-79d700217271?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80)",
         site: "url('./image/site-bg.jpg')",
         about: "url('./src/assets/arifur.jpg')",
         services: "url('./src/assets/programming.jpg')",
-      },
+      }),
     },
   },
   plugins: [],
